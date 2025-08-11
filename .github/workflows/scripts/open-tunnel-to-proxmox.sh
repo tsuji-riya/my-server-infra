6d24d213107b463a13dc953d084efc84d8c1b044
tsuji-riya <tsuji.riya@gmail.com>
2025-08-11 12:35:14 +0900

    ✨️ Update open-tunnel-to-proxmox.sh to set TF_VAR_pm_api_url in GITHUB_ENV

--- .github/workflows/scripts/open-tunnel-to-proxmox.sh
+++ .github/workflows/scripts/open-tunnel-to-proxmox.sh
@@ -64,4 +64,4 @@ echo_to_err ""
 echo_to_err "Log of spawned process:"
 echo_to_err "$(cat "${logfile}")"
 
-export TF_VAR_pm_api_url="https://${tunnel_url}"
+echo "TF_VAR_pm_api_url=https://${tunnel_url}" >> "$GITHUB_ENV"
